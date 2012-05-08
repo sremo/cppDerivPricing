dfa7322d93662b9451c5e28c95555aa315cc5006
Remo Storni <remo.storni@gmail.com>
2012-05-08 00:12:37 +0000

    Initial commit

---
+++ SimpleMCMain1.cpp
@@ -0,0 +1,17 @@
+// requires Random.cpp
+
+
+#include <Random1.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double SimpleMontecarlo1(double Expiry,
+			 double Strike,
+			 double Spot,
+			 double Vol,
+			 double r,
+			 unsigned long NumberOfPaths)
+{
+	double variance
+}	
